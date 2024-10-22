1d856ba9a832da5beee0c65235fc4c94766b5788
Toji <i.im.rimuru@gmail.com>
2024-10-22 21:37:36 +0400

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,15 @@
+#23 Вариант pip install ucimlrepo
+#from ucimlrepo import fetch_ucirepo 
+#  
+## fetch dataset 
+#spambase = fetch_ucirepo(id=94) 
+#  
+## data (as pandas dataframes) 
+#X = spambase.data.features 
+#y = spambase.data.targets 
+#  
+## metadata 
+#print(spambase.metadata) 
+#  
+## variable information 
+#print(spambase.variables) 
