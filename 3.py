4b383276a102a0fdd8dacfc55fa0ea63ce9d9d49
Toji <i.im.rimuru@gmail.com>
2024-10-22 20:47:28 +0400

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,17 @@
+#13 Вариант 
+# pip install ucimlrepo
+
+# from ucimlrepo import fetch_ucirepo 
+  
+# # fetch dataset 
+# energy_efficiency = fetch_ucirepo(id=242) 
+  
+# # data (as pandas dataframes) 
+# X = energy_efficiency.data.features 
+# y = energy_efficiency.data.targets 
+  
+# # metadata 
+# print(energy_efficiency.metadata) 
+  
+# # variable information 
+# print(energy_efficiency.variables) 
